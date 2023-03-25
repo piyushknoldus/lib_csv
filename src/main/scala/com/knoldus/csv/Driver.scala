56caf8d407d5e7be28de268fda8b16a311392a7f
prakhar-flow <x_prakhar@global-e.com>
2023-03-25 15:33:18 +0530

    add fetching from file functionality

--- src/main/scala/com/knoldus/csv/Driver.scala
+++ src/main/scala/com/knoldus/csv/Driver.scala
@@ -1,11 +1,15 @@
 package com.knoldus.csv
 
 object Driver extends App {
-  val reader = CsvReader.fetchFromUrl(
+  private val urlReader = CsvReader.fetchFromUrl(
     "https://www.stats.govt.nz/assets/Uploads/Business-operations-survey/Business-operations-survey-2022/Download-data/business-operations-survey-2022-information-and-communications-technology.csv"
   )
 
-  reader.eachRow { row =>
+  private val fileReader = CsvReader.fetchFromFile(
+    "/home/knoldus/Downloads/business-operations-survey-2022-business-finance.csv"
+  )
+
+  fileReader.eachRow { row =>
     val description = row.getOptionalValue("description")
     val industry = row.getOptionalValue("industry")
     val level = row.getOptionalValue("level")
